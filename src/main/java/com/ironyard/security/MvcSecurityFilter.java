4db310771e2067a2f889edf704af6adfebc87126
nathaniel ellsworth <nathanielellsworth@nathaniels-MacBook-Air.local>
2016-11-12 18:08:01 -0500

    added Security Filter

--- src/main/java/com/ironyard/security/MvcSecurityFilter.java
+++ src/main/java/com/ironyard/security/MvcSecurityFilter.java
@@ -1,7 +1,44 @@
 package com.ironyard.security;
 
+import com.ironyard.data.User;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
 /**
  * Created by nathanielellsworth on 11/5/16.
  */
-public class MvcSecurityFilter {
+public class MvcSecurityFilter implements javax.servlet.Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException{
+        
+    }
+
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = ((HttpServletRequest) servletRequest);
+        HttpServletResponse resp = ((HttpServletResponse) servletResponse);
+        // check session
+        User usr = (User) req.getSession().getAttribute("user");
+        boolean authorized = (usr != null);
+
+        if(authorized){
+            filterChain.doFilter(servletRequest, servletResponse);
+        }else{
+            resp.sendRedirect("/mvc/open/login.jsp");
+        }
+    }
+
+
+    @Override
+    public void destroy(){
+
+    }
+
 }
