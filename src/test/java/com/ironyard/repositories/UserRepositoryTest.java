eedeaa99ffe363f039149053764a4e991b86265f
nathaniel ellsworth <nathanielellsworth@nathaniels-MacBook-Air.local>
2016-11-12 21:01:14 -0500

    built, ran and passed token test

--- src/test/java/com/ironyard/repositories/UserRepositoryTest.java
+++ src/test/java/com/ironyard/repositories/UserRepositoryTest.java
@@ -1,8 +1,16 @@
+package com.ironyard.repositories;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by nathanielellsworth on 11/12/16.
  */
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class UserRepositoryTest {
 
 }
