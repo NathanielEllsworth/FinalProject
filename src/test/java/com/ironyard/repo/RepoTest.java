839aebc1a147827e2be7eaf679f9a0550e8ac574
nathaniel ellsworth <nathanielellsworth@nathaniels-MacBook-Air.local>
2016-11-12 20:59:20 -0500

    updated pom

--- src/test/java/com/ironyard/repo/RepoTest.java
+++
@@ -1,7 +0,0 @@
-package com.ironyard.repo;
-
-/**
- * Created by nathanielellsworth on 11/4/16.
- */
-public class RepoTest {
-}
---
+++ src/test/java/com/ironyard/security/EncryptionTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by nathanielellsworth on 11/12/16.
+ */
+public class EncryptionTest {
+
+}
