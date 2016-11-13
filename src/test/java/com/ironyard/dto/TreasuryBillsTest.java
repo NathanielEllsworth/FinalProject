0ddd7d5fdeae14ce4348c8d3d7a77ea11f35bf40
nathaniel ellsworth <nathanielellsworth@nathaniels-MacBook-Air.local>
2016-11-12 21:40:17 -0500

    added an api quick status code test

--- src/test/java/com/ironyard/dto/TreasuryBillsTest.java
+++ src/test/java/com/ironyard/dto/TreasuryBillsTest.java
@@ -4,10 +4,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
-
-import static jdk.nashorn.internal.objects.NativeString.substring;
-import static org.junit.Assert.*;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Created by nathanielellsworth on 11/4/16.
@@ -17,19 +16,15 @@ import static org.junit.Assert.*;
 public class TreasuryBillsTest {
 
 
+    // just a quick status code test
 
     @Test
-    public void addTreasuryBills(){
-        RestTemplate restTemplate = new RestTemplate();
-        TreasuryBills[] bills = restTemplate.getForObject("http://www.treasurydirect.gov/TA_WS/securities/auctioned?format=json&type=Bill", TreasuryBills[].class);
-        for(int i=0; i<bills.length; i++){
-            String dec = bills[i].getSecurityTerm();
-            if(dec.length()>250){
-                dec = dec.substring(0,250);
-            }
+    public void addQuickTreasuryBillTest() throws IOException{
 
-        }
-
+        URL url = new URL("http://www.treasurydirect.gov/TA_WS/securities/auctioned?format=json&type=Bill");
+        HttpURLConnection http = (HttpURLConnection)url.openConnection();
+        int statusCode = http.getResponseCode();
     }
+
 
 }
