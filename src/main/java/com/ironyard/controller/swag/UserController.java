015b8d1db11f502127fecbc5a0f864c3e8139337
nathaniel ellsworth <nathanielellsworth@nathaniels-MacBook-Air.local>
2016-11-15 09:17:11 -0500

    User swag update

--- src/main/java/com/ironyard/controller/swag/UserController.java
+++ src/main/java/com/ironyard/controller/swag/UserController.java
@@ -1,10 +1,9 @@
 package com.ironyard.controller.swag;
 
+import com.ironyard.data.User;
 import com.ironyard.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,7 +24,47 @@ public class UserController {
 
 
     @RequestMapping(value = "save", method = RequestMethod.POST)
-    
+    public User save(@RequestBody User aUser){
+        log.debug("Begin save:" + aUser);
+        userRepo.save(aUser);
+        User found = userRepo.findOne(aUser.getId());
+        log.debug("End save:" + aUser);
+        return found;
+    }
+
+
+
+    @RequestMapping(value = "update", method = RequestMethod.PUT)
+    public User update(@RequestBody User aUser){
+        log.debug("Begin update:" + aUser);
+        userRepo.save(aUser);
+        User found = userRepo.findOne(aUser.getId());
+        log.debug("End update:" + found);
+        return found;
+    }
+
+
+    @RequestMapping(value = "get/{id}",method = RequestMethod.GET)
+    public User show(@PathVariable Long id){
+        log.debug("Begin show:" + id);
+        User found = userRepo.findOne(id);
+        log.debug("End show:" + found);
+        return found;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
