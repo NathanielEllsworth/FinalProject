d340038e30378343832b3c28c2e7d64bd156fbea
nathaniel ellsworth <nathanielellsworth@nathaniels-MacBook-Air.local>
2016-11-15 12:49:01 -0500

    Swagger User Controller made

--- src/main/java/com/ironyard/controller/swag/UserController.java
+++ src/main/java/com/ironyard/controller/swag/UserController.java
@@ -3,6 +3,8 @@ package com.ironyard.controller.swag;
 import com.ironyard.data.User;
 import com.ironyard.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -54,16 +56,44 @@ public class UserController {
 
 
 
+    @RequestMapping(value = "list", method = RequestMethod.GET)
+    public Iterable<User> listAll(@RequestParam("page") Integer page,
+                                  @RequestParam("size") Integer size,
+                                  @RequestParam(value = "sortby", required = false) String sortby,
+                                  @RequestParam(value = "dir", required = false) Sort.Direction direction){
+
+        log.debug(String.format("Begin listAll (page:%s, size:%s, sortby:%s, dir:%s):", page, size, sortby, direction));
+
+        //Default Sort
+        if(sortby == null){
+            sortby = "displayName";
+        }
+
+        Sort s = new Sort(direction, sortby);
+        PageRequest pr = new PageRequest(page, size, s);
+        Iterable<User> found = userRepo.findAll(pr);
+        log.debug(String.format("End listAll: %s", found));
+
+        return found;
+    }
+
+    @RequestMapping(value = "delete/{id}", method = RequestMethod.DELETE)
+    public User delete(@PathVariable Long id){
+        log.debug(String.format("Begin delete: %s", id));
+        User deleted = userRepo.findOne(id);
+        userRepo.delete(id);
+        log.debug(String.format("End delete: %s", deleted));
+        return deleted;
+    }
 
 
 
-
-
-
-
-
-
-
+    @ExceptionHandler(value = Throwable.class)
+    public String nfeHandler(Throwable e){
+        log.error("Error in UserController", e);
+        return "Something went wrong";
+    }
+    
 
 
 
