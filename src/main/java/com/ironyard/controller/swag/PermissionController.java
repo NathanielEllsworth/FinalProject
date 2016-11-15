86a79b151f0af72328af51d553d4daa355b6ac83
nathaniel ellsworth <nathanielellsworth@nathaniels-MacBook-Air.local>
2016-11-15 13:11:51 -0500

    Perms Swagger Quick-Save

--- src/main/java/com/ironyard/controller/swag/PermissionController.java
+++ src/main/java/com/ironyard/controller/swag/PermissionController.java
@@ -1,9 +1,11 @@
 package com.ironyard.controller.swag;
 
+import com.ironyard.data.Permission;
 import com.ironyard.repositories.PermissionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,5 +26,49 @@ public class PermissionController {
 
 
 
-    
+    @RequestMapping(value = "save", method = RequestMethod.POST)
+    public Permission save(@RequestBody Permission aPermission){
+        permRepo.save(aPermission);
+        return permRepo.findOne(aPermission.getId());
+    }
+
+
+
+    @RequestMapping(value = "update", method = RequestMethod.PUT)
+    public Permission update(@RequestBody Permission aPermission){
+        permRepo.save(aPermission);
+        return permRepo.findOne(aPermission.getId());
+    }
+
+
+    @RequestMapping(value = "get/{id}", method = RequestMethod.GET)
+    public Permission show(@PathVariable Long id){ return permRepo.findOne(id);}
+
+
+
+    @RequestMapping(value = "list", method = RequestMethod.GET)
+    public Iterable<Permission> listAll(@RequestParam(value = "page") Integer page,
+                                        @RequestParam("size") Integer size,
+                                        @RequestParam(value = "sortby", required = false) String sortby,
+                                        @RequestParam(value = "dir", required = false) Sort.Direction direction){
+
+
+        //Sort default
+        if (sortby == null){
+            sortby = "title";
+        }
+
+        //Sort default direction
+        if (direction == null){
+            direction = Sort.Direction.DESC;
+        }
+        Sort s = new Sort(direction, sortby);
+        PageRequest pr = new PageRequest(page, size, s);
+        return permRepo.findAll(pr);
+
+
+    }
+
+
+
 }
