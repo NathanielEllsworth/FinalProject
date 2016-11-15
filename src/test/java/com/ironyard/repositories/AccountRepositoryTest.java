2e53c921de43e133be97f9fe32365dfc9829915c
nathaniel ellsworth <nathanielellsworth@nathaniels-MacBook-Air.local>
2016-11-15 15:28:28 -0500

    Working Backwards from Login

--- src/test/java/com/ironyard/repositories/AccountRepositoryTest.java
+++ src/test/java/com/ironyard/repositories/AccountRepositoryTest.java
@@ -1,32 +1,32 @@
-//package com.ironyard.repositories;
-//
-//import com.ironyard.data.Accounts;
-//import org.junit.Before;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//import static org.junit.Assert.*;
-//
-///**
-// * Created by nathanielellsworth on 11/14/16.
-// */
-//@SpringBootTest
-//@RunWith(SpringRunner.class)
-//public class AccountRepositoryTest {
-//    @Before
-//    public void setUp() throws Exception {
-//        Iterable<Accounts> accounts = acctRepo.findAll();
-//        for(Accounts a:accounts){
-//            acctRepo.delete(a);
-//        }
-//
-//    }
-//
-//
-//    @Autowired
-//    private AccountRepository acctRepo;
-//
-//
-//}
+package com.ironyard.repositories;
+
+import com.ironyard.data.Accounts;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+
+/**
+ * Created by nathanielellsworth on 11/14/16.
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class AccountRepositoryTest {
+    @Before
+    public void setUp() throws Exception {
+        Iterable<Accounts> accounts = acctRepo.findAll();
+        for(Accounts a:accounts){
+            acctRepo.delete(a);
+        }
+
+    }
+
+
+    @Autowired
+    private AccountRepository acctRepo;
+
+
+}
