19da7c42c13447a4637518654070fc4a46b3ae08
nathaniel ellsworth <nathanielellsworth@nathaniels-MacBook-Air.local>
2016-11-15 08:56:15 -0500

    Starting Swagger user controller

--- src/main/java/com/ironyard/controller/swag/UserController.java
+++ src/main/java/com/ironyard/controller/swag/UserController.java
@@ -1,7 +1,12 @@
 package com.ironyard.controller.swag;
 
+import com.ironyard.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * GO TO BED MAN! IT WILL BE HERE TOMORROW!
@@ -11,4 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/rest/user")
 public class UserController {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private UserRepository userRepo;
+
+
+
+    @RequestMapping(value = "save", method = RequestMethod.POST)
+    
+
+
 }
